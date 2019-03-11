be0577aa4360cea7b5b1ce5de1886bfe7927e340
papinaser <papinaser@gmail.com>
2019-03-11 14:07:26 +0330

    Updates

---
+++ precache-manifest.f27da52e1b432e44f186a447c0e1e9cb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "9e70b5763f9fb4bbe6483a2f3e9b59ba",
+    "url": "/shoppingcart/static/media/item5.9e70b576.jpg"
+  },
+  {
+    "revision": "123bf1234de9225f0b2f",
+    "url": "/shoppingcart/static/js/runtime~main.123bf123.js"
+  },
+  {
+    "revision": "426891858fa26078f29a",
+    "url": "/shoppingcart/static/js/main.42689185.chunk.js"
+  },
+  {
+    "revision": "5b6f8482a64d4f8e7f35",
+    "url": "/shoppingcart/static/js/2.5b6f8482.chunk.js"
+  },
+  {
+    "revision": "426891858fa26078f29a",
+    "url": "/shoppingcart/static/css/main.8c354b8d.chunk.css"
+  },
+  {
+    "revision": "5b6f8482a64d4f8e7f35",
+    "url": "/shoppingcart/static/css/2.c6be0f9c.chunk.css"
+  },
+  {
+    "revision": "41126e33554a1d38381d3691480b5d0d",
+    "url": "/shoppingcart/index.html"
+  }
+];
